d1634308b6cc5f8a6952cde4a88fbc6bf8f70c81
Preston L <PrestonLeung@users.noreply.github.com>
2020-08-03 10:33:08 +0200

    Simplifying CommonFunctions.py
    There are some functions here weren't used. Hence commented out but not deleted (in case there are future uses for it).

--- CommonFunctions.py
+++ CommonFunctions.py
@@ -20,31 +20,32 @@ def okFile(file):
 #==========================="getFile"===========================#
 #Opens a file and parses it as a seqRecord file and returns the record.
 
-def getFile(filename):
-    try:
-        OpenFastaFile = open(filename)    
-    except IOError:
-            # print "Error: cannot find {}".format(filename)
-            print(f"Error: cannot find {filename}.")
-    else:
-        record = SeqIO.parse(OpenFastaFile,  "fasta")
-        OpenFastaFile.close        
-        return record
+# def getFile(filename):
+#     try:
+#         OpenFastaFile = open(filename)    
+#     except IOError:
+#             # print "Error: cannot find {}".format(filename)
+#             print(f"Error: cannot find {filename}.")
+#     else:
+#         record = SeqIO.parse(OpenFastaFile,  "fasta")
+#         OpenFastaFile.close        
+#         return record
+    
 #==========================="getFile"===========================#
 
-def createRecord(seq, identity):
+# def createRecord(seq, identity):
     
-    newRecord = SeqRecord(seq)
-    newRecord.id = identity
+#     newRecord = SeqRecord(seq)
+#     newRecord.id = identity
     
-    return newRecord
+#     return newRecord
     
 
 #========================"Self defined Errors and Exceptions"=======================#
 
-class Error(Exception):
-    """Base class for exceptions."""
-    pass
+# class Error(Exception):
+#     """Base class for exceptions."""
+#     pass
     
 #Error classes for handling input errors
 class InputError (Error):
@@ -59,45 +60,33 @@ class InputError (Error):
     def __str__(self):
         return repr(str(self.msg)+str(self.expr))
 
-class RefSeqError(Error):
-    def __init__(self, filename, reason):
-        self.filename = filename
-        self.reason = reason
-    def __str__(self):
-        return repr(str(self.reason) + str(self.filename))    
+# class RefSeqError(Error):
+#     def __init__(self, filename, reason):
+#         self.filename = filename
+#         self.reason = reason
+#     def __str__(self):
+#         return repr(str(self.reason) + str(self.filename))    
         
 
 #========================"EpitopeInfo Structure"=======================#
-class Epitope_Info:
+# class Epitope_Info:
     
-    def __init__(self,allele,rank, start, end):  #parameter sequence omitted for now
-        self.allele = allele
-        #self.sequence = sequence
-        self.rank = rank
-        self.start = start
-        self.end = end
-    def HLA_type(self):
-        return self.allele
-    #def epitope(self):
-    #    return self.sequence
-    def percentile_rank(self):
-        return self.rank
-    def end_position(self):
-        return self.end
-    def start_position(self):
-        return self.start
-
-#==========================="Check if there are 1+ Ref sequences"===========================#
-
-def recordCheck(record, refSeq_Filename):
-    
-    count = 0
-    
-    for item in record:
-        if(count > 0):
-            raise CF.RefSeqError(refSeq_Filename, "There is more than one sequence in ")
-        count += 1
-
+#     def __init__(self,allele,rank, start, end):  #parameter sequence omitted for now
+#         self.allele = allele
+#         #self.sequence = sequence
+#         self.rank = rank
+#         self.start = start
+#         self.end = end
+#     def HLA_type(self):
+#         return self.allele
+#     #def epitope(self):
+#     #    return self.sequence
+#     def percentile_rank(self):
+#         return self.rank
+#     def end_position(self):
+#         return self.end
+#     def start_position(self):
+#         return self.start
 
 
 #========================"Main"=======================#
